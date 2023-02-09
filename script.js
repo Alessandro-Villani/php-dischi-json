821af2ab223127db2b5d63a3a97e36a7bdcabe9d
Alessandro-Villani <alessvill@libero.it>
2023-02-09 16:22:47 +0100

    added ccontent from api

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 console.log('VUE OK', Vue);
 
+const apiUri = 'http://localhost/php-dischi-json/api.php';
+
 const app = Vue.createApp({
 data(){
     return {
-        
+        disks: []
     }
+},
+methods: {
+    getDisks(){
+        axios.get(apiUri).then(res => {
+            this.disks = res.data;
+        })
+    }
+},
+mounted(){
+    this.getDisks();
 }
 });
 
